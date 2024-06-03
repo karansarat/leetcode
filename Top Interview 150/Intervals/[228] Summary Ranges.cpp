62512c08c14ac2879387871c5ad74ef4a7661492
karan <karansarat@ymail.com>
2024-06-03 15:00:07 +0800

    Formatting change

--- Top Interview 150/Intervals/[228] Summary Ranges.cpp
+++ Top Interview 150/Intervals/[228] Summary Ranges.cpp
@@ -10,14 +10,14 @@ public:
             if (nums[e] + 1 == nums[e+1]) e++;
             else {
                 if(s != e) str = to_string(nums[s]) + "->" + to_string(nums[e]);
-                else str=to_string(nums[e]);
+                else str = to_string(nums[e]);
                 ans.push_back(str);
-                s=e+1;
-                e=s;
+                s = e + 1;
+                e = s;
             }
         }
         if(s != e) str = to_string(nums[s]) + "->" + to_string(nums[e]);
-        else str=to_string(nums[e]);
+        else str = to_string(nums[e]);
         ans.push_back(str);
         return ans;
     }
