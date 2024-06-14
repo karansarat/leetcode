13e85d2ed67b50bea3182fd38f875323dfc1a586
karan <karansarat@ymail.com>
2024-06-14 15:17:35 +0800

    Commit 211

---
+++ Top Interview 150/Trie/[211] Design Add and Search Words Data Structure.py
@@ -0,0 +1,37 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+        
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for w in word:
+            if w not in curr.children:
+                curr.children[w] = TrieNode()
+            curr = curr.children[w]  
+        curr.is_word = True    
+
+    def search(self, word: str) -> bool:
+        stack = [(self.root,0)]
+        while stack:
+            curr,idx = stack.pop()
+            if idx == len(word):
+                if curr.is_word:
+                    return True
+            elif word[idx] == '.':
+                for child in curr.children.values():
+                    stack.append((child,idx+1))
+            elif word[idx] in curr.children:
+                stack.append((curr.children[word[idx]], idx+1))
+        return False        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
