6c2b3677317c4547d3f1c9b12c861eb4f7169301
karan <karansarat@ymail.com>
2024-06-20 12:14:36 +0800

    Commit 72

---
+++ Top Interview 150/Multidimensional DP/[72] Edit Distance.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minDistance(self, s1: str, s2: str) -> int:
+        def dp(i, j):
+            if i == 0: return j
+            if j == 0: return i
+            if s1[i-1] == s2[j-1]:
+                return dp(i-1, j-1)
+            return min(dp(i-1, j), dp(i, j-1), dp(i-1, j-1)) + 1
+        
+        return dp(len(s1), len(s2))
