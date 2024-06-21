57b1d9602e290d4ce6cc75da4a411d17519f20d6
karan <karansarat@ymail.com>
2024-06-21 22:06:43 +0800

    Commit 30

---
+++ Top Interview 150/Sliding Window/[30] Substring with Concatenation of All Words.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        int totalWords = words.length;
+        int wordLength = words[0].length();
+
+        Map<String, Integer> wordsMap = new HashMap<>(); 
+        Arrays.stream(words).forEach(word -> wordsMap.merge(word, 1, Integer::sum));
+        
+        for (int i = 0; i < wordLength; i++) {
+            Map<String, Integer> currMap = new HashMap<>();
+            for (int j = i; j <= s.length() - wordLength; j += wordLength) {
+                String curr = s.substring(j, j + wordLength);
+                if (j - i >= totalWords * wordLength) {
+                    int idx = j - totalWords * wordLength;
+                    String prev = s.substring(idx, idx + wordLength);
+                    currMap.put(prev, currMap.getOrDefault(prev, 1) - 1);
+                    if (currMap.get(prev) == 0) currMap.remove(prev);
+                }
+                currMap.put(curr, currMap.getOrDefault(curr, 0) + 1);
+                if (currMap.equals(wordsMap)) {
+                    System.out.println(j);
+                    result.add(j - (totalWords - 1) * wordLength);
+                }
+            }
+        }
+        return result;
+    }
+}
