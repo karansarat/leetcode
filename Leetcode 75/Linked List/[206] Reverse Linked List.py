a4c0a869d3e9c960c04df84bf0e9ff12c7b4a88c
karan <karansarat@ymail.com>
2024-06-29 13:18:19 +0800

    Commit 206

---
+++ Leetcode 75/Linked List/[206] Reverse Linked List.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        node = None
+        while head:
+            tmp = head.next
+            head.next = node
+            node = head
+            head = tmp
+        return node
